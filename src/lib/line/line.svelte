2635f04533a6ffe0e37d5425ecd837496db7f5d1
Ciro Angeleri <19916218+CiroAngeleri@users.noreply.github.com>
2024-10-11 23:46:33 +0200

    feat: First line component

---
+++ src/lib/line/line.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import * as d3 from 'd3';
+	type Props = {
+		x: any;
+		y: any;
+		data: any;
+	};
+	let { data = [], x, y }: Props = $props();
+
+	const line = d3.line().x(x).y(y);
+</script>
+
+<path fill="none" stroke="steelblue" stroke-width="1.5" d={line(data)} />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,23 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Line from '$lib/line/line.svelte';
+	import * as d3 from 'd3';
+	import data from '../__mocks__/data.json';
+
+	type Datum = (typeof data)[number];
+
+	const width = 300;
+	const height = 300;
+
+	const xDomain = d3.extent(data, (d: Datum) => new Date(d.date)) as [Date, Date];
+	const yDomain = [0, d3.max(data, (d: Datum) => d.close)] as [0, number];
+	const xRange = width;
+	const xScale = d3.scaleUtc(xDomain, [0, xRange]);
+	const yScale = d3.scaleLinear(yDomain, [height, 0]);
+
+	const x = (d: Datum) => xScale(new Date(d.date));
+	const y = (d: Datum) => yScale(d.close);
+</script>
+
+<svg {width} {height} viewBox="0 0 {width} {height}" style:max-width="100%" style:height="auto">
+	<Line {x} {y} {data} />
+</svg>
